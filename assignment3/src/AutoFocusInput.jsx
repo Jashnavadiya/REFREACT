8e455fc5e3c1e19c3021d2f33640e8295a034c6a
Jashnavadiya <jashnavadiya07@gmail.com>
2024-07-15 19:17:54 +0530

    3

--- assignment3/src/AutoFocusInput.jsx
+++ assignment3/src/AutoFocusInput.jsx
@@ -1,6 +1,3 @@
-/*   Assignment 3: Using Refs for Auto Focus**
- Create a functional component named "AutoFocusInput" to automatically focus on an input field using a ref.
-Problem Statement:**: Create a functional component named "AutoFocusInput.": Implement a ref to automatically focus on an input field when the component mounts.: Display an input field in the component and verify that it gains focus as soon as the component is rendered. */
 
 import React, { useRef, useEffect } from 'react';
 
